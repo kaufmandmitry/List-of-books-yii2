760a08d3dd2f5884797ff38ea206ac2c67fda0a9
Dmitry Kaufman <KaufmanDmitry@gmail.com>
2016-06-03 17:10:21 +0600

    Переименовал контроллер в Crud для ясности

--- modules/admin/controllers/CrudController.php
+++ modules/admin/controllers/CrudController.php
@@ -7,7 +7,7 @@ use yii\web\Controller;
 /**
  * Default controller for the `admin` module
  */
-class DefaultController extends Controller
+class CrudController extends Controller
 {
     /**
      * Renders the index view for the module
@@ -16,5 +16,10 @@ class DefaultController extends Controller
     public function actionIndex()
     {
         return $this->render('index');
+    }
+
+    public function actionHello()
+    {
+        echo "Hello Dmitry!=)";
     }
 }
